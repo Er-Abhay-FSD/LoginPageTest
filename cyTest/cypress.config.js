32f312f701dbd679340ba8605c5bdafcbde20a62
Abhay Singh <128287855+Er-Abhay-FSD@users.noreply.github.com>
2023-06-24 19:52:53 +0530

    Add files via upload

---
+++ cyTest/cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'd1dhxa',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
